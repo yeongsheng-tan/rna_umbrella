a6cb3f4c14cac3858d44ce540391ac72d7a88bf7
Yeong Sheng, Tan <yeongsheng.tan@ruckuswireless.com>
2017-04-18 10:49:02 +0800

    fix: correct json payload response

--- apps/rna_web/lib/rna_web/controllers/switch_controller.ex
+++ apps/rna_web/lib/rna_web/controllers/switch_controller.ex
@@ -2,7 +2,7 @@ defmodule Rna.Web.SwitchController do
   use Rna.Web, :controller
 
   def show(conn, %{"ip" => ip}) do
-    switch_info = Rna.Snmp.get_switch_info(ip) |> Poison.encode!
+    switch_info = Rna.Snmp.get_switch_info(ip)
     json conn, switch_info
   end
 end
